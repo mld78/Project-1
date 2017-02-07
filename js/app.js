a46ac10fe1ae57c33570e9bec7efcc4bb3fd7b21
Matt Dias <mld78@case.edu>
2017-02-07 14:31:22 -0800

    started JS logic

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 console.log('Connected to this site')
 
-$(document).ready(function() {
+$().ready(function() {
     ship = $('#spaceShip')
     area = $('#gameArea').
     start()
-})
-
+}
+)
 function start() {
     gameState = obstaclesCleared = score = 0
     currentShipPosition = { x: 80, y:100, h:40, w:50 }
-    ship.css({left:cPos.x, top:cPos.y, width:cPos.w, height:cPos.h, rotate:0})
+    ship.css({left:currentShipPosition.x, top:currentShipPosition.y, width:currentShipPosition.w, height:currentShipPosition.h, rotate:0})
     $('.obstacles').remove()
     $('#instructions').show()
 }
